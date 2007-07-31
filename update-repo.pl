648e8314bef615e92dc76cf3570c89d4909b794f
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-07-31 17:54:31 +0000

    more distros, whee

--- update-repo.pl
+++ update-repo.pl
@@ -8,12 +8,19 @@ use File::Path;
 use IO::Handle;
 
 my @trees = qw(stable unstable);
-my @oses  = qw(rhel4 rhel5);
+my @oses  = qw(fc4 fc5 fc6 fc7 rhel3 rhel4 rhel5 suse9 suse10);
 
 my $descriptions = {
 	common => 'RPMs Common to All OpenNMS Architectures',
+	fc4    => 'Fedora Core 4',
+	fc5    => 'Fedora Core 5',
+	fc6    => 'Fedora Core 6',
+	fc7    => 'Fedora Core 7',
+	rhel3  => 'RedHat Enterprise Linux 3.x and CentOS 3.x',
 	rhel4  => 'RedHat Enterprise Linux 4.x and CentOS 4.x',
 	rhel5  => 'RedHat Enterprise Linux 5.x and CentOS 5.x',
+	suse9  => 'SuSE Linux 9.x',
+	suse10 => 'SuSE Linux 10.x',
 };
 
 my $index = IO::Handle->new();
@@ -47,12 +54,18 @@ for my $tree (@trees) {
 			"$tree/$dir",
 		) == 0 or die "unable to run createrepo: $!";
 
-		if ($tree eq 'unstable') {
-			write_repofile('stable', $dir, "repofiles/opennms-$tree-$dir.repo", 0);
-			write_repofile($tree,    $dir, "repofiles/opennms-$tree-$dir.repo", 1);
-		} else {
-			write_repofile($tree,    $dir, "repofiles/opennms-$tree-$dir.repo", 0);
-		}
+		my $repofile = IO::Handle->new();
+		open($repofile, ">repofiles/opennms-$tree-$dir.repo") or die "unable to write to repofiles/opennms-$tree-$dir.repo: $!";
+		print $repofile <<END;
+[opennms-$tree-$dir]
+name=$descriptions->{$dir} RPMs ($tree)
+baseurl=http://yum.opennms.org/$tree/$dir
+mirrorlist=http://yum.opennms.org/mirrorlists/$tree-$dir.txt
+failovermethod=priority
+gpgcheck=1
+gpgkey=http://yum.opennms.org/OPENNMS-GPG-KEY
+END
+		close($repofile);
 
 		if (not -f "mirrorlists/$tree-$dir.txt") {
 			my $mirrorlist = IO::Handle->new();
@@ -69,27 +82,6 @@ print $index <<END;
  </body>
 </html>
 END
-
-sub write_repofile {
-	my $tree     = shift;
-	my $dir      = shift;
-	my $filename = shift;
-	my $append   = shift || 0;
-
-	my $repofile = IO::Handle->new();
-	open($repofile, ($append? '>>' : '>') . $filename) or die "unable to write to $filename: $!";
-	print $repofile <<END;
-[opennms-$tree-$dir]
-name=$descriptions->{$dir} RPMs ($tree)
-baseurl=http://yum.opennms.org/$tree/$dir
-mirrorlist=http://yum.opennms.org/mirrorlists/$tree-$dir.txt
-failovermethod=priority
-gpgcheck=1
-gpgkey=http://yum.opennms.org/OPENNMS-GPG-KEY
-
-END
-	close($repofile);
-}
 
 close ($index);
 
